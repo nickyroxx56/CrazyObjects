750a31afa2ecf77f3aa1a2104c89bf505a1758d3
Connor St. Louis <033423@hsc.on.ca>
2015-10-22 14:45:11 -0400

    swag

--- CrazyObjects/src/crazyobjects/Student.java
+++ CrazyObjects/src/crazyobjects/Student.java
@@ -1,5 +1,21 @@
+package crazyobjects;
+
 
 public class Student
 {
+    int stuNumber;
+    String name;
+    
+    -stuNumber:  int
+-name:  String
+-myLocker:  Locker
+-myJacket:  Jacket
+-books[]:  Book
+
+    stuNumber
+name
+myLocker
+myJacket
+books[]
 
 } // end Student class
