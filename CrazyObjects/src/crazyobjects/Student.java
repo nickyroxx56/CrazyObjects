5d23e5e484f2935b1829a1944629be2db5220281
Connor St. Louis <033423@hsc.on.ca>
2015-10-22 14:59:32 -0400

    no message

--- CrazyObjects/src/crazyobjects/Student.java
+++ CrazyObjects/src/crazyobjects/Student.java
@@ -5,17 +5,19 @@ public class Student
 {
     int stuNumber;
     String name;
-    
-    -stuNumber:  int
--name:  String
--myLocker:  Locker
--myJacket:  Jacket
--books[]:  Book
-
-    stuNumber
-name
-myLocker
-myJacket
-books[]
-
+    Locker myLocker = new Locker();
+    Jacket myJacket = new Jacket();
+    Book books = new Book();
+    public Student(String n, int s)  {
+        name = n;
+        stuNumber = s;
+    }
+    public void sendToOffice(String reason){
+        System.out.println("The student has been sent to the office because:");
+        System.out.println(reason);
+    } //prints out the reason why the student was sent to the office
+    @Override
+    public String toString() {
+        return "Student{" + "stuNumber=" + stuNumber + ", name=" + name + ", myLocker=" + myLocker + ", myJacket=" + myJacket + ", books=" + books + '}';
+    }
 } // end Student class
