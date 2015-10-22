5a517e9b08a6cc1593e7bbb28a75dd17b05bb358
Nicky Spencer <031060@hsc.on.ca>
2015-10-22 14:50:49 -0400

    no message

--- CrazyObjects/src/crazyobjects/Book.java
+++ CrazyObjects/src/crazyobjects/Book.java
@@ -1,5 +1,28 @@
-
+package crazyobjects;
 public class Book
-{
+{public String title;
+public String course;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" + "title=" + title + ", course=" + course + '}';
+    }
+
 
 } // end Book class
--- CrazyObjects/src/crazyobjects/Locker.java
+++ CrazyObjects/src/crazyobjects/Locker.java
@@ -1,4 +1,58 @@
-
+package crazyobjects;
 public class Locker
 {
+    public Jacket studentJacket;
+    public int number;
+    public Student owner;
+    public Book[] books;
+
+    public Jacket getStudentJacket() {
+        return studentJacket;
+    }
+
+    public void setStudentJacket(Jacket studentJacket) {
+        this.studentJacket = studentJacket;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public Student getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Student owner) {
+        this.owner = owner;
+    }
+
+    public Book[] getBooks() {
+        return books;
+    }
+
+    public void setBooks(Book[] books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Locker{" + "studentJacket=" + studentJacket + ", number=" + number + ", owner=" + owner + ", books=" + books + '}';
+    }
+
+    public Locker(int number, Book[] books) {
+        this.number = number;
+        this.books = books;
+        this.owner = new Student();
+        this.studentJacket = new Jacket();
+        
+    }
+    
+    public Jacket takeOutJacket() {
+        return .getStudentjacket;
+    }
+    
 } // end Locker class
